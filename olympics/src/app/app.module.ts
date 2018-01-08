153e191b2615590b0795e7e62121b1623ea3681b
Bourgeois Guillaume <guillaume.bourgeois-ducournau@etudiant.univ-reims.fr>
2018-01-08 16:48:45 +0100

    ionic

--- olympics/src/app/app.module.ts
+++ olympics/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { GoogleMaps } from '@ionic-native/google-maps';
-
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -13,6 +11,7 @@ import { StatisticsPage } from '../pages/statistics/statistics';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { GoogleMaps } from '@ionic-native/google-maps';
 
 @NgModule({
   declarations: [
--- olympics/src/pages/map/map.ts
+++ olympics/src/pages/map/map.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions,
-  CameraPosition, MarkerOptions, Marker} from '@ionic-native/google-maps';
+import { ToastController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions, GoogleMapsAnimation,
+  CameraPosition, MarkerOptions, Marker, MyLocation } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
 })
 export class MapPage {
-  
+
   mapReady: boolean = false;
   map: GoogleMap;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              private googleMaps: GoogleMaps) {
+              public toastCtrl: ToastController, private googleMaps: GoogleMaps) {
   }
 
   ionViewDidLoad() {
@@ -23,7 +24,7 @@ export class MapPage {
   loadMap() {
     // Create a map after the view is loaded.
     // (platform is already ready in app.component.ts)
-    this.map = this.googleMaps.create('map_canvas', {
+    /*this.map = this.googleMaps.create('map_canvas', {
       camera: {
         target: {
           lat: 43.0741704,
@@ -32,12 +33,59 @@ export class MapPage {
         zoom: 18,
         tilt: 30
       }
+    });*/
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    // Wait the maps plugin is ready until the MAP_READY event
+    this.map.one(GoogleMapsEvent.MAP_READY)
+      .then( () => {
+
+      console.log('Map is ready!');
+
+        // Now you can use all methods safely.
+        this.map.addMarker({
+            title: 'Ionic',
+            icon: 'blue',
+            animation: 'DROP',
+            position: {
+              lat: 43.0741904,
+              lng: -89.3809802
+            }
+          })
+          .then(marker => {
+            marker.on(GoogleMapsEvent.MARKER_CLICK)
+              .subscribe(() => {
+                alert('clicked');
+              });
+          });
     });
 
+    /*
     // Wait the maps plugin is ready until the MAP_READY event
     this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
       this.mapReady = true;
-    });
+    });*/
+  }
+
+  onButtonClick2() {
+    if (!this.mapReady)
+    {
+      this.loadMap();
+      this.showToast('loading map');
+    }
+    return;
   }
 
   onButtonClick() {
