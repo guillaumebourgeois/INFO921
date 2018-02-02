96af95d5e03e6ae53450fa876141336df64ef96b
Bourgeois Guillaume <guillaume.bourgeois-ducournau@etudiant.univ-reims.fr>
2018-02-02 11:07:36 +0100

    bcrypt

---
+++ olympics/src/providers/api.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable() export class API
+{
+
+  private apiUrl : string = "http://10.129.15.160:8080/";
+
+  constructor(public http: HttpClient) {
+    console.log('Hello RestServiceProvider Provider');
+  }
+
+  public auth() {
+    return new Promise (resolve => {
+    })
+  }
+
+  public getUsers() {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl+'/users').subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  public addUser(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.apiUrl+'/users', JSON.stringify(data))
+        .subscribe(res => {
+          resolve(res);
+        }, (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  /*this.http.post(this.apiUrl'/users', JSON.stringify(data), {
+    headers: new HttpHeaders().set('Authorization', 'my-auth-token'),
+    params: new HttpParams().set('id', '3'),
+  })
+  .subscribe(res => {
+    resolve(res);
+  }, (err) => {
+    reject(err);
+  });*/
+
+}
