8c4c68a9e9fa04a55c5a68acaa6605a7d66d5fa5
taoukilite <carchar4321>
2018-02-02 08:39:51 +0100

    Implemented user OAuth2

---
+++ OlympicsAPI/Olympics/src/main/java/com/olympics/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.olympics.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.olympics.entities.User;
+
+@Repository
+public interface UserDao extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
